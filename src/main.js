7c0e240ef002b0e1e949a5510794f601e060acd5
YoMoCo <keungyeunghong@gmail.com>
2024-08-15 20:02:13 +1000

    Week4

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
 import { createApp } from 'vue'
 import App from './App.vue'
 // import './style.css'
 
-createApp(App).mount('#app')
+app.mount('#app')
+
