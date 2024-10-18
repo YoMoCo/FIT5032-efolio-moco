e13699bda2b81ea41863495d31305e29dff87f27
YoMoCo <keungyeunghong@gmail.com>
2024-10-19 00:00:48 +1100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,22 @@ import vue from '@vitejs/plugin-vue'
 
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//     // base: '/FIT5032-efolio-moco/', 
+//     plugins: [
+//       vue(),
+//       vueDevTools(),
+//     ],
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url))
+//       }
+//     },
+//     publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/' 
+//   })
+
 export default defineConfig({
-    // base: '/FIT5032-efolio-moco/', 
+    base: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/', 
     plugins: [
       vue(),
       vueDevTools(),
@@ -29,5 +43,5 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/' 
   })
+  
