1f56f4cf90213d05c0b683776bcd1f9dc806d576
YoMoCo <keungyeunghong@gmail.com>
2024-10-18 22:29:18 +1100

    fix1.2

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/'
 })
 
 
