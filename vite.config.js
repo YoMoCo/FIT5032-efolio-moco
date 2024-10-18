a4dac40098d10637977d6a30c833b5cde025523f
YoMoCo <keungyeunghong@gmail.com>
2024-10-19 00:13:29 +1100

    fix2.0

--- vite.config.js
+++ vite.config.js
@@ -18,22 +18,7 @@ import vue from '@vitejs/plugin-vue'
 
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//     base: '/FIT5032-efolio-moco/', 
-//     plugins: [
-//       vue(),
-//       vueDevTools(),
-//     ],
-//     resolve: {
-//       alias: {
-//         '@': fileURLToPath(new URL('./src', import.meta.url))
-//       }
-//     }
-//     // publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/' 
-//   })
-
 export default defineConfig({
-    // publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/', 
     base: '/FIT5032-efolio-moco/', 
     plugins: [
       vue(),
@@ -43,6 +28,21 @@ export default defineConfig({
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    },
+    }
+    // publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/' 
   })
+
+// export default defineConfig({
+//     // publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/', 
+//     base: '/FIT5032-efolio-moco/', 
+//     plugins: [
+//       vue(),
+//       vueDevTools(),
+//     ],
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url))
+//       }
+//     },
+//   })
   
